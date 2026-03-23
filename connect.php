1364623d47534c8bc73db8a8699c0c6d2342ece4
Nanami / 七海 <85467106+nanamiiwashere@users.noreply.github.com>
2026-03-23 22:36:46 +0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -16,7 +16,7 @@ if ($db_url){
     $pass = $parsed['pass'];
 } else {
     #fallback local
-    $host = getenv('DB_HOST') ?: 'db.sdtenasvwqdkunztwcyy.supabase.co';
+    $host = getenv('DB_HOST') ?: 'pooler.sdtenasvwqdkunztwcyy.supabase.co';
     $port = getenv('DB_PORT') ?: '6543';
     $dbname = getenv('DB_NAME') ?: 'postgres';
     $user = getenv('DB_USER') ?: 'postgres';
