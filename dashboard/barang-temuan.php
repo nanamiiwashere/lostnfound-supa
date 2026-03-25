c5da06fd84a24e3f027a347336580e81e6fadbc3
Nanami / 七海 <85467106+nanamiiwashere@users.noreply.github.com>
2026-03-25 23:05:45 +0700

    Add files via upload

--- dashboard/barang-temuan.php
+++ dashboard/barang-temuan.php
@@ -2,7 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) session_start();
 require_once '../connect.php';
 require_once '../Auth/auth3thparty.php';
-require_once './Core/supabase-handler.php';
+require_once '../Core/supabase-handler.php';
 requireLogin();
 $u = currentUser();
 $activePage = 'temuan';
